219a3b42385b471d9c1c6c96f48e9536153a94cb
irfan340 <61534088+irfan340@users.noreply.github.com.>
2024-07-05 06:29:48 +0600

    insert a database and post in /spots

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const spotCollection=client.db('spotDB').collection('spot')
+
+
+    app.post('/spots',async(req,res)=>{
+      let newSpot=req.body
+      console.log(newSpot)
+      let result=await spotCollection.insertOne(newSpot)
+      res.send(result)
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
