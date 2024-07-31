67093c3fa2aaa1ca80b281b5e1a29bca86ef0867
irfan340 <61534088+irfan340@users.noreply.github.com.>
2024-07-31 06:23:22 +0600

    post operation has been done for user data

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 //Mongdb 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bspxqoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,8 +28,9 @@ async function run() {
     await client.connect();
     
     const spotCollection=client.db('spotDB').collection('spot')
+    const userCollection= client.db('spotDB').collection('user')
 
-
+    // spot related apis
     app.post('/spots',async(req,res)=>{
       let newSpot=req.body
       console.log(newSpot)
@@ -37,7 +38,58 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/spots', async(req,res)=>{
+      const cursor=spotCollection.find()
+      const result= await cursor.toArray()
+      res.send(result)
+    })
+
+    app.delete('/spots/:id', async(req,res)=>{
+      let id=req.params.id
+      let query={_id: new ObjectId(id)}
+      let result= await spotCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/spots/:id', async(req,res)=>{
+      let id=req.params.id
+      const query={_id: new ObjectId(id)}
+      let result= await spotCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.put('/spots/:id', async(req,res)=>{
+      const id= req.params.id 
+      let filter ={_id: new ObjectId(id)}
+      let options={ upsert:true}
+      let updatedSpot=req.body
+      let spot={
+        $set:{
+          name :updatedSpot.name,
+          email:updatedSpot.email,
+          country:updatedSpot.country,
+          spotName:updatedSpot.spotName,
+          location:updatedSpot.location,
+          cost:updatedSpot.cost,
+          time:updatedSpot.time,
+          seasonality:updatedSpot.seasonality,
+          visitors:updatedSpot.visitors,
+          description:updatedSpot.description,
+          photo:updatedSpot.photo
+        }
+      }
+      let result=await spotCollection.updateOne(filter, spot, options)
+      res.send(result)
+    })
     
+    // user related apis
+    app.post('/user',  async(req,res)=>{
+      const user=req.body
+      console.log(user)
+      const result =await userCollection.insertOne(user)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
